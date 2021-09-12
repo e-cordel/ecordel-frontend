b69527d3156771e2ac043b9022c359c54613e443
Mario Sousa <m_santos_sousa@hotmail.com>
2021-09-12 16:51:17 +0100

    Fix default api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://ecordel-restapi.herokuapp.com'
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://ecordel-restapi.herokuapp.com/api/v1/'
 });
 
 export default api;
