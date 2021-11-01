6c1372b4beb706e82f20c31a7c30f00c4e2350f0
Samuel Rodrigues <samuelclerod@gmail.com>
2021-11-01 10:38:02 -0300

    feature:: style cordel detail and skeleton

---
+++ src/components/CordelViewer/index.tsx
@@ -0,0 +1,74 @@
+import { Card, Container, Skeleton, Typography as T, useTheme } from "@material-ui/core";
+
+
+export interface CordelDetailsInterface {
+  id: string;
+  author: {
+    id: number;
+    name: string;
+    about: string;
+    email: string;
+  }
+  title: string;
+  description: string;
+  content: string;
+  published: boolean;
+  tags: Array<string>;
+  xilogravuraUrl: string;
+}
+
+type CordelViewerProps = {
+  cordel: CordelDetailsInterface
+}
+
+const toParagraph = (fullText: string) => {
+  return fullText.split('/n/n').map((block) => (<p>{block}</p>))
+}
+
+export const CordelViewer = ({ cordel }: CordelViewerProps) => {
+
+  const theme = useTheme();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <T variant="h3">{cordel.title}</T>
+      <T variant="subtitle1">{cordel.author.name}</T>
+      <Card sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: theme.spacing(3)
+      }}>
+        <img
+          src={cordel.xilogravuraUrl}
+          alt={cordel.title}
+          style={{
+            flex: 1,
+          }} />
+      </Card>
+
+      {toParagraph(cordel.content)}
+    </Container>);
+}
+
+export const CordelViewerSkeleton = () => (
+  <Container component="main" maxWidth="xs">
+    <T variant="h3"><Skeleton variant="text" /></T>
+    <T variant="subtitle1"><Skeleton variant="text" /></T>
+    <Skeleton variant="rectangular" height={550} />
+    <p>
+      <Skeleton variant="text" />
+      <Skeleton variant="text" />
+      <Skeleton variant="text" />
+      <Skeleton variant="text" />
+    </p>
+    <p>
+      <Skeleton variant="text" />
+      <Skeleton variant="text" />
+      <Skeleton variant="text" />
+      <Skeleton variant="text" />
+    </p>
+
+  </Container>
+
+)
--- src/pages/cordels/CordelDetails.tsx
+++ src/pages/cordels/CordelDetails.tsx
@@ -1,39 +1,19 @@
-import { Container } from "@material-ui/core"
+import { Container } from "@material-ui/core";
 import { useParams } from "react-router"
+import { CordelDetailsInterface, CordelViewer, CordelViewerSkeleton } from "../../components/CordelViewer";
 import { useFetch } from "../../hooks/useFetch"
 
-interface Cordel {
-  id: string;
-  author: {
-    id: number;
-    name: string;
-    about: string;
-    email: string;
-  }
-  title: string;
-  description: string;
-  content: string;
-  published: boolean;
-  tags: Array<string>;
-  xilogravuraUrl: string;
-}
+export default function CordelDetails() {
 
-export default function CordelDetails(){
+  const { id } = useParams<{ id: string }>()
 
-  const {id} = useParams<{id: string}>()
-
-  const {data: cordel} = useFetch<Cordel,Error>(`cordels/${id}`);
+  const { data: cordel } = useFetch<CordelDetailsInterface, Error>(`cordels/${id}`);
+  // const cordel = null;
   console.log(cordel)
 
-  if(!cordel) return <p>Loadding...</p>
+  if (!cordel) return <CordelViewerSkeleton />
 
   return (<Container>
-
-    <p>Clicked on {id}</p>
-    <p><strong>Autor: </strong>{cordel.author.name}</p>
-    <p><strong>Title: </strong>{cordel.title}</p>
-    
-    <p><strong>Xilogravura: </strong>{cordel.xilogravuraUrl}</p>
-    <p><strong>Texto: </strong>{cordel.content}</p>
-    </Container>)
+    <CordelViewer cordel={cordel} />
+  </Container>)
 }
