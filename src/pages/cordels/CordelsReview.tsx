0263146b0d219e6b4a8406c6a944b5fbaab52acc
Samuel Rodrigues <samuelclerod@gmail.com>
2021-11-06 12:39:25 -0300

    add review initial page

---
+++ src/pages/cordels/CordelsReview.tsx
@@ -0,0 +1,10 @@
+import { Container } from '@material-ui/core'
+import React from 'react'
+
+export default function CordelsReview() {
+  return (
+    <Container>
+      Cordel review
+    </Container>
+  )
+}
