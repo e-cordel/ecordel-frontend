7214bb5ecd0b06a4acd1a497e81453ed1c87950d
Mario Sousa <m_santos_sousa@hotmail.com>
2023-06-10 10:35:46 +0100

    fix home link

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -7,7 +7,6 @@ import {
   Typography,
 } from "@mui/material";
 import { FiSun, FiMoon } from "react-icons/fi";
-import { Link } from "react-router-dom";
 import { useColorMode } from "../../hooks/useColorMode";
 import { AuthButton } from "../AuthButton";
 import Nav from "./Nav";
@@ -21,7 +20,7 @@ export default function Navbar() {
         <Container>
           <Toolbar sx={{ flexWrap: 'wrap' }}>
             <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
-              <Button color="inherit" href="/" LinkComponent={Link}>e-cordel</Button>
+              <Button color="inherit" href="/">e-cordel</Button>
             </Typography>
             
             <Nav />
